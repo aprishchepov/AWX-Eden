827e0ef5592127dce1a415f1fce6d485a0d64468
mizaki <jinxybob@hotmail.com>
2012-06-22 22:19:12 +0100

    Replace win slashes for linux as win understand these also. Fixes artist logos with windows.

--- js/lib.xbmc.js
+++ js/lib.xbmc.js
@@ -317,9 +317,9 @@ var xbmc = {};
 			$.extend(settings, options);
 			
 			if (settings.path.startsWith('stack://')) {
-				settings.path = settings.path.replace(/\\/g, "\\\\").substring(8, settings.path.indexOf(","));
+				settings.path = settings.path.replace(/\\/g, "/").substring(8, settings.path.indexOf(","));
 			}
-			var path = settings.path.replace(/\\/g, "\\\\").substring(0, settings.path.lastIndexOf("/"));
+			var path = settings.path.replace(/\\/g, "/").substring(0, settings.path.lastIndexOf("/"));
 
 			path += '/' + settings.type + '.png';
 			
